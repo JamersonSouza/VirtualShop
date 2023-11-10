00f9eea84781bdeac4c42965afba6fb6465ebbc9
jamersonsouza <jamersonsouza@outlook.com>
2023-11-09 22:17:25 -0300

    Manipulando a exibição dos cards de acordo com o modo escolhido

--- src/app/shared/components/pages/UIComponents/product-box/product-box.component.ts
+++ src/app/shared/components/pages/UIComponents/product-box/product-box.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
   templateUrl: './product-box.component.html'
 })
-export class ProductBoxComponent {
+export class ProductBoxComponent implements OnInit{
+
+  @Input() withMode = false;
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+  }
 
 }
