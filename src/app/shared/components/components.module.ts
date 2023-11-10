f6b0bc1287d870cebb898f8417c45431958505e0
jamersonsouza <jamersonsouza@outlook.com>
2023-11-09 21:40:02 -0300

    inclusão da box de produtos no grid list da home

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductHeaderComponent } from './pages/UIComponents/product-header/product-header.component';
 import { FiltersComponent } from './pages/UIComponents/filters/filters.component';
+import { ProductBoxComponent } from './pages/UIComponents/product-box/product-box.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, HomeComponent, ProductHeaderComponent, FiltersComponent],
+  declarations: [HeaderComponent, HomeComponent, ProductHeaderComponent, FiltersComponent, ProductBoxComponent],
   imports: [
     CommonModule,
     MaterialModulesModule
--- src/app/shared/components/pages/home/home.component.ts
+++ src/app/shared/components/pages/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROW_HEIGHT: {[id:number]: number} = {1: 400, 3: 335, 4:350};
+
 @Component({
   selector: 'app-home',
   templateUrl:  './home.component.html',
 })
 export class HomeComponent implements OnInit{
 
-  cols = 0;
+  cols = 3;
   category: string | undefined
+  rowHeight = ROW_HEIGHT[this.cols];
 
   constructor(){
 
@@ -18,6 +21,8 @@ export class HomeComponent implements OnInit{
 
   onColumsCountChange(colsNumber : number): void{
     this.cols = colsNumber;
+    this.rowHeight = ROW_HEIGHT[this.cols];
+    console.log('aq:', this.cols, 'aq2: ', this.rowHeight)
   }
 
   getCategory(categorySelected: string): void{
