8ecc4508e21b57f76bc5a6571eb0fb0e47eae568
jamersonsouza <jamersonsouza@outlook.com>
2023-11-19 10:24:03 -0300

    listagem das categorias da api no componente

--- src/app/shared/components/pages/UIComponents/filters/filters.component.ts
+++ src/app/shared/components/pages/UIComponents/filters/filters.component.ts
@@ -1,20 +1,33 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { FakestoreService } from 'src/app/services/fakestore.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html'
 })
-export class FiltersComponent implements OnInit{
+export class FiltersComponent implements OnInit, OnDestroy{
 
   @Output() showCategories = new EventEmitter<string>();
+  
+  categorias : Array<string> | undefined;
+  categoriasSub : Subscription | undefined;
 
-  categorias = ['camisas', 'tênis'];
 
-  constructor() {
+  constructor(private fakeStore : FakestoreService) {
     
+  }
+  ngOnDestroy(): void {
+    if(this.categoriasSub){
+      this.categoriasSub.unsubscribe();
+    }
   }
 
   ngOnInit(): void {
+   this.categoriasSub = this.fakeStore.getAllCategories()
+    .subscribe( (response) => {
+        this.categorias = response;
+    });
   }
 
   showCategory(category: string): void {
